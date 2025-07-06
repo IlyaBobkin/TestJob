2964a3f94e07f3f325671e2a17ed9d0e666f42c9
IlyaBobkin <103562689+IlyaBobkin@users.noreply.github.com>
2025-07-06 14:46:25 +0300

    mainform changes

--- TechJob/MainForm.Designer.cs
+++ TechJob/MainForm.Designer.cs
@@ -34,12 +34,13 @@
             button3 = new Button();
             button4 = new Button();
             button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 16F);
-            button1.Location = new Point(188, 82);
+            button1.Location = new Point(208, 96);
             button1.Name = "button1";
             button1.Size = new Size(192, 35);
             button1.TabIndex = 0;
@@ -60,58 +61,72 @@
             // 
             // button2
             // 
+            button2.Enabled = false;
             button2.Font = new Font("Segoe UI", 16F);
-            button2.Location = new Point(6, 136);
+            button2.Location = new Point(26, 150);
             button2.Name = "button2";
             button2.Size = new Size(173, 39);
             button2.TabIndex = 0;
             button2.Text = "Прочитать из:";
             button2.UseVisualStyleBackColor = true;
-            button2.Click += button1_Click;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
             button3.Font = new Font("Segoe UI", 16F);
-            button3.Location = new Point(188, 136);
+            button3.Location = new Point(208, 150);
             button3.Name = "button3";
             button3.Size = new Size(93, 39);
             button3.TabIndex = 0;
             button3.Text = "Файл";
             button3.UseVisualStyleBackColor = true;
-            button3.Click += button1_Click;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
             button4.Font = new Font("Segoe UI", 16F);
-            button4.Location = new Point(287, 136);
+            button4.Location = new Point(307, 150);
             button4.Name = "button4";
             button4.Size = new Size(93, 39);
             button4.TabIndex = 0;
             button4.Text = "БД";
             button4.UseVisualStyleBackColor = true;
-            button4.Click += button1_Click;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
             button5.Font = new Font("Segoe UI", 16F);
-            button5.Location = new Point(188, 191);
+            button5.Location = new Point(208, 205);
             button5.Name = "button5";
             button5.Size = new Size(192, 35);
             button5.TabIndex = 0;
             button5.Text = "Ручной ввод";
             button5.UseVisualStyleBackColor = true;
-            button5.Click += button1_Click;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightCoral;
+            button6.Font = new Font("Segoe UI", 16F);
+            button6.Location = new Point(239, 282);
+            button6.Name = "button6";
+            button6.Size = new Size(192, 45);
+            button6.TabIndex = 0;
+            button6.Text = "Выход";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Gainsboro;
-            ClientSize = new Size(393, 252);
+            BackColor = Color.PapayaWhip;
+            ClientSize = new Size(443, 339);
             Controls.Add(label1);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
+            Controls.Add(button6);
             Controls.Add(button5);
             Controls.Add(button1);
             Name = "MainForm";
@@ -129,5 +144,6 @@
         private Button button3;
         private Button button4;
         private Button button5;
+        private Button button6;
     }
 }
--- TechJob/MainForm.cs
+++ TechJob/MainForm.cs
@@ -1,7 +1,11 @@
+using System.Drawing;
+
 namespace TechJob
 {
     public partial class MainForm : Form
     {
+        
+
         public MainForm()
         {
             InitializeComponent();
@@ -14,12 +18,41 @@ namespace TechJob
 
         private void button1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
